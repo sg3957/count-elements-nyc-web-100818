d032aeafd4bbde8b46bcfb62249a801b0bcca8af
Sydney Grant <sg3957@nyu.edu>
2018-10-09 20:09:55 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,7 +3,7 @@ def count_elements(array)
   new_hash = {}
   array.each do |key, value|
     if new_hash.include?(key)
-      new_hash[key].value += 1 
+      new_hash[key] += 1 
     else
      new_hash.merge!(key: 1)
     end
